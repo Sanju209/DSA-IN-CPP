cd3e895e7cd54549337571de99582eff207e564f
Sanju209 <sanjay.c2096@gmail.com>
2025-02-27 16:11:11 +0000

    4th lecture

--- patternsl4/triangle.cpp
+++ patternsl4/triangle.cpp
@@ -1,5 +1,3 @@
-//Without using extextra variable num
-
 #include <iostream>
 using namespace std;
 
@@ -9,9 +7,9 @@ int main() {
     cout << "Enter the height : ";
     cin >> n;
 
-    for(int i = 1; i <= n*n; i+=n) {
-        for(int j = i; j<= i+n-1; j++) {
-            cout << j << " ";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i+1; j++) {
+            cout << "*";
         }
         cout << "\n";
     } 
